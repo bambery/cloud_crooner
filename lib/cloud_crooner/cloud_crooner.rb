fe3744c909ffcaf8fe93c579e0990d3136be81b9
bambery <lwszolek@gmail.com>
2013-08-08 00:54:36 -0500

    delete config class since it was merged into the base class

--- lib/cloud_crooner/cloud_crooner.rb
+++ lib/cloud_crooner/cloud_crooner.rb
@@ -171,6 +171,13 @@ module CloudCrooner
       $stdout.puts msg
     end
 
+    def fog_options
+      options = { :provider => provider, :aws_access_key_id => aws_access_key_id, :aws_secret_access_key => aws_secret_access_key, :region => region }
+    end
+
+
+    end
+
     private
 
     def provider
